24adeed207d4ee3c1e710d5cd2bd0fefd4bb6310
Mayank Nayal <mayanknayal5306069@gmail.com>
2024-11-12 10:12:53 +0530

    header renovated

--- src/components/header/firstHeader/FirstHeader.jsx
+++ src/components/header/firstHeader/FirstHeader.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './FirstHeader.css';
+import { Link } from 'react-router-dom';
+import {HashLink} from 'react-router-hash-link';
 
 const FirstHeader = () => {
     return (
         <header className="Firstheader">
-            <h1><i>Carbon Footprint Tracker</i></h1>
+            <h1><Link to="/" style={{ color: 'white', textDecoration: 'none' }}><i>Carbon Footprint Tracker</i></Link></h1>
             <nav>
                 <ul>
-                    <li><a href="#features">Features</a></li>
-                    <li><a href="#how-it-works">How It Works</a></li>
-                    <li><a href="#cta">Get Started</a></li>
+                    <li><HashLink to="/#features">Features</HashLink></li>
+                    <li><HashLink to="/#how-it-works">How It Works</HashLink></li>
+                    <li><HashLink to="/#cta">Get Started</HashLink></li>
                 </ul>
             </nav>
         </header>
