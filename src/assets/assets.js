9810dd070417b505a3b2241bde5c2ec026cb7882
Mayank Nayal <mayanknayal5306069@gmail.com>
2024-11-13 18:45:03 +0530

    sidebar done in homepage

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,7 @@
 import howitworks from "./images/howworks.jpg";
+import profile from "./images/profile.jpg";
 
 export const neededImages = {
     howitworks,
+    profile,
 }
--- src/components/homeComponents/Sidebar.jsx
+++ src/components/homeComponents/Sidebar.jsx
@@ -1,22 +1,25 @@
 // Sidebar.js
 import React from 'react';
 import { FaHome, FaChartPie, FaDollarSign, FaBoxes, FaUser, FaEnvelope, FaCog, FaSignOutAlt } from 'react-icons/fa';
+import { neededImages } from '../../assets/assets';
+
 
 const Sidebar = () => (
     <div className="sidebar">
-        <div className="profile">
-        <img src="https://via.placeholder.com/80" alt="Profile" />
+        <div className="sidebar-profile">
+            <h2>Carbon Footprint Tracker</h2>
+        <img src={neededImages.profile} alt="Profile" />
         <h3>Mark Johnson</h3>
-        <p>Welcome Back</p>
+        <p>Welcome Back!</p>
         </div>
-        <ul className="menu">
+        <ul className="sidebar-menu">
         <li><FaHome /> Dashboard</li>
         <li><FaChartPie /> Statistics</li>
-        <li><FaDollarSign /> Payment</li>
-        <li><FaBoxes /> Transactions</li>
-        <li><FaUser /> Products</li>
-        <li><FaEnvelope /> Messages</li>
-        <li><FaCog /> Settings</li>
+        <li><FaDollarSign /> Donation</li>
+        <li><FaBoxes /> Developer's Corner</li>
+        <li><FaUser /> Edit Profile</li>
+        <li><FaEnvelope /> Contact</li>
+        <li><FaCog /> Developer's Corner</li>
         <li><FaSignOutAlt /> Log Out</li>
         </ul>
     </div>
