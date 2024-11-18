da102226c99dd36cdc3d21dcb50661d2962596ec
Mayank Nayal <mayanknayal5306069@gmail.com>
2024-11-19 00:18:11 +0530

    made some changes in chart.js

--- src/components/homeComponents/Chart.jsx
+++ src/components/homeComponents/Chart.jsx
@@ -1,24 +1,29 @@
-// Chart.js
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
 
 const data = [
-    { name: 'Mon', uv: 20 },
-    { name: 'Tue', uv: 30 },
-    { name: 'Wed', uv: 40 },
-    { name: 'Thu', uv: 25 },
-    { name: 'Fri', uv: 35 },
-    { name: 'Sat', uv: 20 },
-    { name: 'Sun', uv: 10 },
+    { name: 'Mon', black: 20, beige: 20 },
+    { name: 'Tue', black: 30, beige: 10 },
+    { name: 'Wed', black: 40, beige: 0 },
+    { name: 'Thu', black: 25, beige: 15 },
+    { name: 'Fri', black: 35, beige: 5 },
+    { name: 'Sat', black: 20, beige: 20 },
+    { name: 'Sun', black: 10, beige: 30 },
 ];
 
 const Chart = () => (
-    <BarChart width={300} height={200} data={data}>
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Bar dataKey="uv" fill="#8884d8" />
-    </BarChart>
+    <div className="chart-container">
+        <BarChart width={700} height={300} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            {/* Bottom Black Layer */}
+            <Bar dataKey="black" stackId="a" fill="#1c1c1c" />
+            {/* Top Beige Layer */}
+            <Bar dataKey="beige" stackId="a" fill="#f8f7f4" />
+        </BarChart>
+    </div>
 );
 
 export default Chart;
